03530087f1ad63811c7aa60ea1ada6e4e298838e
Aleksandr <aleksandrignashov@42gmail.com>
2021-11-06 19:11:11 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//alert("hello");
+//prompt();\
+//const result = confirm("Are you here?")
+//console.log(result);
+//const answer = +prompt("How old are you", "");
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt("How old are you", "");
+//answers[1] = prompt("How your name", "");
+//
+//console.log(typeof(answers));
+
+//const category = "toys";
+//
+//console.log(`https://someurl.com/${category}/5`);
+//
+//const user = "alex";
+//
+//alert(`hello, ${user}`);
+
+//console.log('arr' + " - object");
+//console.log(4 + "5"); // need console.log(4 + +" - object");
+//
+//let incr = 10,
+//    decr = 10;
+//
+////++incr;
+////--decr;
+//
+//console.log(++incr);
+//console.log(--decr);
+//
+//console.log((2 + 2) * 2 != 8);
+//
+//const isChecked = false,
+//      isClose = false;
+//console.log(isChecked || isClose);
+
+
