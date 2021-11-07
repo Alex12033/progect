622784ba937c8ba8dcd705e3614fce1950af49c7
Aleksandr <aleksandrignashov@42gmail.com>
2021-11-07 08:54:21 +0200

    add cycles and branch operator

--- test.js
+++ test.js
@@ -1,21 +1,70 @@
-const howMany = +prompt("How many films you see?");
+//const howMany = +prompt("How many films you see?");
+//
+//const personalMovieDB = {
+//    count: howMany,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    private: false
+//};
+//
+//const a = prompt("Last movies which you see", ""),
+//      b = +prompt("Please to grade film from 1 to 10"),
+//      c = prompt("Last movies which you see", ""),
+//      d = +prompt("Please to grade film from 1 to 10");
+//
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
+//
+//console.log(personalMovieDB);
 
-const personalMovieDB = {
-    count: howMany,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+//if (0) {
+//    console.log('OK!');
+//} else {
+//    console.log('error');
+//}
 
-const a = prompt("Last movies which you see", ""),
-      b = +prompt("Please to grade film from 1 to 10"),
-      c = prompt("Last movies which you see", ""),
-      d = +prompt("Please to grade film from 1 to 10");
+//const num = 5;
+//
+//if (num < 49) {
+//    console.log("error");
+//} else if (num > 100) {
+//    console.log("a lot");
+//} else {
+//    console.log("ok");
+//}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//(num === 50) ? console.log('ok') : console.log("error");
 
-console.log(personalMovieDB);
+//switch (num) {
+//    case 49:
+//        console.log('not');
+//        break;
+//    case 500:
+//        console.log('not');
+//        break;
+//    case 50:
+//        console.log("ok");
+//        break;
+//    default: console.log("at not time");
+//}
 
+//let num = 50;
 
+//while (num < 55) {
+//    console.log(num);
+//    num++;
+//}
+
+//do {
+//    console.log(num);
+//    num++;
+//} while (num < 55);
+
+//for (let i = 1; i < 10; i++) {
+//    if (i === 6) {
+//        //break;
+//        continue;
+//    }
+//    console.log(i);
+//}
